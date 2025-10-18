3ada313b8f94b7ae7fea0c25039574f4c5d7e47e
rahul-verse <rahulkumar07info@gmail.com>
2025-10-18 12:37:20 +0530

    warning removed

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,4 +1,4 @@
-import React,{useState , useEffect} from "react";
+import React,{useState} from "react";
 import "../../styles/HeaderStyles.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
